0b0cef16d2ec4acacde47447d4007cf245131edb
Arie Satya Pamungkas <arie.pamungkas@bfi.co.id>
2024-02-15 10:40:10 +0700

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+R
+"src/main/java/com/bfi/app/App.java,e/3/e343844e25c8180ce68d4ca581a1934300571aa8
---
+++ src/main/java/com/bfi/app/App.java
@@ -0,0 +1,15 @@
+package com.bfi.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void display(String param){
+        System.out.println("You parameter is " + param);
+    }
+
+    public void someFunc(){
+        System.out.println("Testing.someFunc");
+    }
+}
